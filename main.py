323d50d592048b6bfcc84ca644c0f8b99dd217db
Felipe Linares <Carlos.Linares.2019@upb.edu.co>
2019-08-19 12:38:17 -0500

    Mi Primera Modificacion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-a=imput("Ingresar numero")
-c=imput("Ingresar numero")
+a=imput("Ingresar numero a")
+c=imput("Ingresar numero c")
+b=imput("Ingresar numero b")
+l=b*b
 x=a*c
 d=2*a
 print("el valor de la multiplicacion es", x)
 print("valor del doble de a", d)
+print("el cuadrado de b es igual a", l)
